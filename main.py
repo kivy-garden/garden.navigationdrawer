925a441af10b7cabd3a9ba2e46ae0212d8719c75
Alexander Taylor <alexanderjohntaylor@gmail.com>
2013-08-24 01:19:29 +0100

    Sync main.py with navigationdrawer example

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.image import Image
+from kivy.uix.widget import Widget
 from kivy.core.window import Window
+from kivy.metrics import dp
 
 from navigationdrawer import NavigationDrawer
 
@@ -24,22 +26,47 @@ class ExampleApp(App):
         side_panel.add_widget(Button(text='Another button'))
         navigationdrawer.add_widget(side_panel)
 
-        label_head = '[b]Example label filling main panel[/b]\n\n[color=ff0000](pull from left to right!)[/color]\n\nIn this example, the left panel is a simple boxlayout menu, and this main panel is a BoxLayout with a label and example image.\n\n'
-        riker = "Some days you get the bear, and some days the bear gets you. I am your worst nightmare! You enjoyed that. When has justice ever been as simple as a rule book? For an android with no feelings, he sure managed to evoke them in others. Flair is what marks the difference between artistry and mere competence. Worf, It's better than music. It's jazz. The game's not big enough unless it scares you a little. We finished our first sensor sweep of the neutral zone. Your head is not an artifact! What? We're not at all alike!"
+        label_head = '''[b]Example label filling main panel[/b]\n\n[color=ff0000](pull from left to right!)[/color]\n\nIn this example, the left panel is a simple boxlayout menu, and this main panel is a BoxLayout with a label and example image.\n\nSeveral preset layouts are available (see buttons below), but users may edit every parameter for much more customisation.'''
         main_panel = BoxLayout(orientation='vertical')
-        label = Label(text=label_head+riker, font_size='15sp',
-                           markup=True, valign='top', padding=(-30,-30))
-        main_panel.add_widget(label)
+        label_bl = BoxLayout(orientation='horizontal')
+        label = Label(text=label_head, font_size='15sp',
+                           markup=True, valign='top')
+        label_bl.add_widget(Widget(size_hint_x=None, width=dp(10)))
+        label_bl.add_widget(label)
+        label_bl.add_widget(Widget(size_hint_x=None, width=dp(10)))
+        main_panel.add_widget(Widget(size_hint_y=None, height=dp(10)))
+        main_panel.add_widget(label_bl)
+        main_panel.add_widget(Widget(size_hint_y=None, height=dp(10)))
         main_panel.add_widget(Image(source='red_pixel.png', allow_stretch=True,
-                                    keep_ratio=False))
+                                    keep_ratio=False, size_hint_y=0.2))
         navigationdrawer.add_widget(main_panel)
         label.bind(size=label.setter('text_size'))
+
+        def set_anim_type(name):
+            navigationdrawer.anim_type = name
+        modes_layout = BoxLayout(orientation='horizontal')
+        modes_layout.add_widget(Label(text='preset\nanims:'))
+        slide_in_button = Button(text='slide_in')
+        slide_in_button.bind(on_press=lambda j: set_anim_type('slide_in'))
+        fade_in_button = Button(text='fade_in')
+        fade_in_button.bind(on_press=lambda j: set_anim_type('fade_in'))
+        reveal_button = Button(text='reveal_\nfrom_\nbelow')
+        reveal_button.bind(on_press=lambda j: set_anim_type('reveal_from_below'))
+        modes_layout.add_widget(slide_in_button)
+        modes_layout.add_widget(fade_in_button)
+        modes_layout.add_widget(reveal_button)
+        main_panel.add_widget(modes_layout)
+
+
         button = Button(text='toggle NavigationDrawer state (animate)', size_hint_y=0.2)
         button.bind(on_press=lambda j: navigationdrawer.toggle_state())
         button2 = Button(text='toggle NavigationDrawer state (jump)', size_hint_y=0.2)
         button2.bind(on_press=lambda j: navigationdrawer.toggle_state(False))
+        button3 = Button(text='toggle _main_above', size_hint_y=0.2)
+        button3.bind(on_press=navigationdrawer.toggle_main_above)
         main_panel.add_widget(button)
         main_panel.add_widget(button2)
+        main_panel.add_widget(button3)
 
         return navigationdrawer
 
