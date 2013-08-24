c6df5ceb338e753a69f477caf7e89eff9263a68b
Alexander Taylor <alexanderjohntaylor@gmail.com>
2013-08-24 17:18:11 +0100

    Reorganised image names

--- navigationdrawer.py
+++ navigationdrawer.py
@@ -541,9 +541,9 @@ class NavigationDrawer(StencilView):
         if self.separator_image:
             return self.separator_image
         if self._main_above:
-            return 'navigationdrawer_gradient_rtol3.png'
+            return 'navigationdrawer_gradient_rtol.png'
         else:
-            return 'navigationdrawer_gradient_ltor3.png'
+            return 'navigationdrawer_gradient_ltor.png'
 
 if __name__ == '__main__':
     from kivy.base import runTouchApp
